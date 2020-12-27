7d1ee400f7ffb0670385ad44f45123beecadbc00
Henrik Grimler <henrik@grimler.se>
2020-12-27 18:48:08 +0100

    libdispatch: fix build with ndk r22
    Patch from @buttaface. This commit is responsible:
    https://github.com/aosp-mirror/platform_bionic/commit/76e2b15

--- packages/libdispatch/build.sh
+++ packages/libdispatch/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@buttaface"
 _VERSION=5.3
 TERMUX_PKG_VERSION=1:${_VERSION}
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${_VERSION}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=6805b555aab65d740fccaa99570fd29b32efa6c310fd42524913e44509dc4969
 TERMUX_PKG_DEPENDS="libc++, libblocksruntime"
