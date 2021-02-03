5901b533ef1bdb10b5bdd26b276689c121bd27d6
wiserain <mail275@gmail.com>
2021-02-03 23:42:19 +0900

    rclone: mod to 1.54.0-1

--- packages/rclone/build.sh
+++ packages/rclone/build.sh
@@ -1,10 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://rclone.org/
 TERMUX_PKG_DESCRIPTION="rsync for cloud storage"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_MAINTAINER="@wiserain"
 TERMUX_PKG_VERSION=1.54.0
-TERMUX_PKG_SRCURL=https://github.com/rclone/rclone/releases/download/v${TERMUX_PKG_VERSION}/rclone-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=95f952dc059b842bd40338458b77657f7b5a1680c4ca837a3adcf83b63c8fda1
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=https://github.com/wiserain/rclone.git
+TERMUX_PKG_GIT_BRANCH=v$TERMUX_PKG_VERSION-$TERMUX_PKG_REVISION
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
@@ -15,7 +16,7 @@ termux_step_make_install() {
 	ln -sf "$PWD" .gopath/src/github.com/rclone/rclone
 	export GOPATH="$PWD/.gopath"
 
-	go build -v -o rclone
+	go build -v -o rclone --ldflags "-s -X github.com/rclone/rclone/fs.Version=$TERMUX_PKG_GIT_BRANCH"
 
 	# XXX: Fix read-only files which prevents removal of src dir.
 	chmod u+w -R .
@@ -23,4 +24,5 @@ termux_step_make_install() {
 	cp rclone $TERMUX_PREFIX/bin/rclone
 	mkdir -p $TERMUX_PREFIX/share/man/man1/
 	cp rclone.1 $TERMUX_PREFIX/share/man/man1/
+	zip $HOME/termux-packages/debs/rclone-$TERMUX_PKG_GIT_BRANCH-termux-$TERMUX_ARCH.zip rclone
 }
